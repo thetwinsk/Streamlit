ff55ac0f5383f855905a527e9458de5333840509
thetwinsk <92984634+thetwinsk@users.noreply.github.com>
2022-10-25 13:56:26 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -31,58 +31,6 @@ from sklearn.utils.validation import column_or_1d
 y = pd.read_csv('y_loan.csv')
 y = column_or_1d(y, warn=False)
 
-# def get_dataset(dataset_name):
-#     if dataset_name == "Iris":
-#         data = datasets.load_iris()
-#     elif dataset_name == "Breast Cancer":
-#         data = datasets.load_breast_cancer()
-#     else:
-#         data = datasets.load_wine()
-#     X = data.data
-#     y = data.target
-#     return X, y
-
-# X, y = get_dataset(dataset_name)
-# st.write("shape of dataset", X.shape)
-# st.write("number of classes", len(np.unique(y)))
-#
-def add_parameter_ui(clf_name):
-    params = dict()
-    # if clf_name == "Logistic Regression":
-    #     pass
-    if clf_name == "Naive Bayes -Bernoulli":
-        pass
-    if clf_name == "KNN":
-        K = st.sidebar.slider("K", 1, 15)
-        params["K"] = K
-    elif clf_name == "SVM":
-        C = st.sidebar.slider("C", 0.01, 10.0)
-        params["C"] = C
-    elif clf_name == "Random Forest":
-        max_depth = st.sidebar.slider("max_depth", 2, 15)
-        n_estimators = st.sidebar.slider("n_estimators", 1, 100)
-        params["max_depth"] = max_depth
-        params["n_estimators"] = n_estimators
-    return params
-#
-#params = add_parameter_ui(classifier_name)
-#
-def get_classifier(clf_name, params):
-    # if clf_name == "Logistic Regression":
-    #     pass
-    if clf_name == "Naive Bayes -Bernoulli":
-        clf = BernoulliNB()
-    if clf_name == "KNN":
-        clf = KNeighborsClassifier(n_neighbors=params["K"])
-    elif clf_name == "SVM":
-        clf = SVC(C=params["C"])
-    elif clf_name == "Random Forest":
-        clf = RandomForestClassifier(n_estimators=params["n_estimators"],
-                                     max_depth=params["max_depth"], random_state=42)
-    return clf
-#
-#clf = get_classifier(classifier_name, params)
-#
 # Classification
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -101,8 +49,8 @@ loanterm = st.number_input('Which term (months) would you like to tend? ',
                            min_value=36, max_value=480, step=12)
 
 X_input = [[income, coincome, loanamount, loanterm]]
-X_input_scaled = min_max_scaler.fit(X_input)
-y_result = clf.predict(X_input_scaled)
+#X_input_scaled = min_max_scaler.fit(X_input)
+y_result = clf.predict(X_input)
 st.write('Your result is...')
 if y_result == 1:
     st.write('Approval.')
